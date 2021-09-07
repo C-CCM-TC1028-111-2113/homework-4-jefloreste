308db2757a55822b642c1ebd9f7c33c49e10a2ff
jefloreste <71605758+jefloreste@users.noreply.github.com>
2021-09-06 20:27:37 -0500

    Update exercise.py

--- assignments/25TrianguloAsteriscos/src/exercise.py
+++ assignments/25TrianguloAsteriscos/src/exercise.py
@@ -1,8 +1,13 @@
-
 def main():
-    #Escribe tu código debajo de esta línea
-    pass
+    return()
 
+rows = int(input("Input the # of rows wanted: "))
+k = 2 * rows - 2
 
-if __name__=='__main__':
-    main()
+for i in range(0, rows):
+    for j in range(0, k):
+        print(end=" ")
+    k = k - 2
+    for j in range(0, i + 1):
+        print("* ", end="")
+    print("")
